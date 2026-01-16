2d5e7ae918344069426a187d53ec2973b2260f84
robsonmonteirolacerda <robsonmonteirolacerda@gmail.com>
2026-01-15 23:30:57 -0300

    feat: Função onAddTaskSubmit gera id aleatório

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -43,7 +44,7 @@ function App() {
 
   function onAddTaskSubmit(title, description) {
     const newTask = {
-      id: tasks.length + 2,
+      id: v4(),
       title,
       description,
       isCompleted: false,
