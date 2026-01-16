e43b2c63390884181c5a4788b35e1e146c9e095b
robsonmonteirolacerda <robsonmonteirolacerda@gmail.com>
2026-01-16 00:14:59 -0300

    feat: Adicionado a página do detalhes da tarefa.

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,6 +1,15 @@
 import { ChevronRightIcon, DeleteIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 function Tasks(props) {
+  const navigate = useNavigate();
+
+  function onSeeDetailsClick(task) {
+    const query = new URLSearchParams();
+    query.set("title", task.title);
+    query.set("description", task.description);
+    navigate(`/task?${query.toString()}`);
+  }
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
       {props.tasks.map((task) => (
@@ -14,7 +23,10 @@ function Tasks(props) {
           >
             {task.title}
           </button>
-          <button className="bg-slate-400 p-2 rounded-md text-white">
+          <button
+            onClick={() => onSeeDetailsClick(task)}
+            className="bg-slate-400 p-2 rounded-md text-white"
+          >
             <ChevronRightIcon />
           </button>
 
---
+++ src/pages/TaskPage.jsx
@@ -0,0 +1,22 @@
+import { useSearchParams } from "react-router-dom";
+
+function TaskPage() {
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Detalhes da Tarefa
+        </h1>
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskPage;
